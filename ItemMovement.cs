49d9eb94c3aede4f022a879dc018ed35cb9ce3e1
davidrogel <dabido236376@gmail.com>
2018-03-12 14:04:41 +0100

    all

---
+++ ItemMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed = 0;
+    private Vector3 dir;
+
+    private bool used;
+
+    private Transform myTransform;
+
+    private Color myColor;
+
+    private SpawnManager manager;
+
+	void Start ()
+    {
+        used = false;
+
+        myTransform = transform;
+
+        dir = (Vector3.zero - myTransform.position);
+
+        Vector3 rand = new Vector3(Random.Range(0, 2), Random.Range(0, -2), 0f);
+
+        dir += rand;
+
+        dir.Normalize();
+
+        myColor = GetComponent<SpriteRenderer>().color;
+
+        manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<SpawnManager>();
+	}
+	
+	void Update ()
+    {
+        myTransform.Translate(dir * speed * Time.deltaTime);
+	}
+
+    void OnMouseDown()
+    {
+        if (myColor == Color.black && used == false)
+        {
+            used = true;
+            manager.AddScore();
+        }
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float radius = 12;
+    public float timeBetweenSpawns = 1;
+
+    public float minSpeed = 0.3f;
+    public float maxSpeed = 0.75f;
+
+    public GameObject prefab;
+    public Sprite[] sprites;
+
+    public Color[] colors;
+    
+    public bool run;
+
+    private List<Vector3> startPositions;
+
+    void Start ()
+    {
+        CreateStartPositions();
+        StartCoroutine(Spawn());
+	}
+
+    void CreateStartPositions()
+    {
+        startPositions = new List<Vector3>();
+        float TWO_PI = Mathf.PI * 2;
+        for (float i = 0; i < TWO_PI; i += 0.1f)
+        {
+            Vector3 randomPosition = new Vector3();
+            randomPosition.x = Mathf.Cos(i);
+            randomPosition.y = Mathf.Sin(i);
+            randomPosition *= radius;
+
+            startPositions.Add(randomPosition);            
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(timeBetweenSpawns);
+
+        GameObject go = Instantiate(prefab, startPositions[Random.Range(0, startPositions.Count)], Quaternion.identity);
+
+        go.GetComponent<SpriteRenderer>().color = (Random.Range(0, 100) <= 35)
+            ?
+                Color.black
+            :
+                colors[Random.Range(0, colors.Length - 1)];
+
+        ItemMovement goItem = go.GetComponent<ItemMovement>();
+        goItem.speed = Random.Range(minSpeed, maxSpeed);
+
+        if(run)
+            StartCoroutine(Spawn());
+    }
+}
